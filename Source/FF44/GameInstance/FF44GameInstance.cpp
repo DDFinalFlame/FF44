c5b4cc4532e5919189814bdd64cb02a158dcadfe
세영 오 <kgcaD@kgcaschool.com>
2025-08-26 10:43:22 +0900

    Feat : "Interactable Actor 생성"

--- Source/FF44/GameInstance/FF44GameInstance.cpp
+++ Source/FF44/GameInstance/FF44GameInstance.cpp
@@ -2,27 +2,52 @@
 
 
 #include "GameInstance/FF44GameInstance.h"
+#include "Kismet/KismetMathLibrary.h"
 
 void UFF44GameInstance::AddGold(int32 Amount)
 {
-    Gold += Amount;
+    Gold = FMath::Max(0, Gold + Amount);
 }
 
 bool UFF44GameInstance::SpendGold(int32 Amount)
 {
+    if (Amount <= 0) return true;
     if (Gold < Amount) return false;
-    Gold -= Amount; return true;
+
+    Gold -= Amount;
+    return true;
 }
 
 void UFF44GameInstance::AddItem(const FItemInstance& Item)
 {
-    Stash.Add(Item);
+    int32 Index = Stash.IndexOfByPredicate([&](const FItemInstance& I)
+        {
+            return I.ItemId == Item.ItemId && I.Price == Item.Price;
+        });
+
+    if (Index != INDEX_NONE)
+    {
+        Stash[Index].Quantity += Item.Quantity;
+    }
+    else
+    {
+        Stash.Add(Item);
+    }
 }
 
 bool UFF44GameInstance::RemoveItemById(FName ItemId)
 {
-    int32 Idx = Stash.IndexOfByPredicate([&](const FItemInstance& I) { return I.ItemId == ItemId; });
-    if (Idx != INDEX_NONE) { Stash.RemoveAt(Idx); return true; }
+    int32 Index = Stash.IndexOfByPredicate([&](const FItemInstance& I)
+        {
+            return I.ItemId == ItemId;
+        });
+
+    if (Index != INDEX_NONE)
+    {
+        Stash.RemoveAt(Index);
+        return true;
+    }
+
     return false;
 }
 
