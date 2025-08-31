0ad8f5a107da8245ad4e12ef1d061dd561955ad4
세영 오 <osy940717@gmail.com>
2025-08-31 15:39:15 +0900

    버그 걸려서 기존 브렌치 파기 후 새로 만듬

--- Source/FF44/Interactable/FF44InteractableActor.h
+++ Source/FF44/Interactable/FF44InteractableActor.h
@@ -9,17 +9,22 @@
 
 class UStaticMeshComponent;
 class USphereComponent;
+class UWidgetComponent;
+class UUserWidget;
 
 UCLASS()
 class FF44_API AFF44InteractableActor : public AActor, public IFF44Interactable
 {
-	GENERATED_BODY()
-	
-public:	
-	AFF44InteractableActor();
+    GENERATED_BODY()
+
+public:
+    AFF44InteractableActor();
 
 protected:
-	virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
+
+public:
+    virtual void Tick(float DeltaSeconds) override;
 
 public:
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Interactable")
@@ -28,7 +33,43 @@ public:
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Interactable")
     USphereComponent* InteractRange;
 
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Interactable|UI")
+    UWidgetComponent* PromptWidget;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interactable|UI")
+    TSubclassOf<UUserWidget> PromptWidgetClass;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interactable|UI")
+    float PromptDistance = 100.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interactable|UI")
+    float PromptHeight = 80.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interactable|UI")
+    bool bYawOnly = true;
+
+public:
+    UFUNCTION(BlueprintImplementableEvent, Category = "Interactable")
+    void BP_OnFocus(AActor* Interactor);
+
+    UFUNCTION(BlueprintImplementableEvent, Category = "Interactable")
+    void BP_OnUnfocus(AActor* Interactor);
+
+    UFUNCTION()
+    void OnRangeBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& Sweep);
+
+    UFUNCTION()
+    void OnRangeEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
     virtual bool CanInteract_Implementation(AActor* Interactor) const override;
     virtual void Interact_Implementation(AActor* Interactor) override;
+
+    UFUNCTION(BlueprintCallable, Category = "Interactable|UI")
+    void SetPromptVisible(bool bVisible);
+
+    void UpdatePromptFacing();
+
+private:
+    TWeakObjectPtr<AActor> CurrentInteractor;
 
 };
