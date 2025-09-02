4cd98b6dad71bce0c8150f4a4ccbd12938ce6742
세영 오 <osy940717@gmail.com>
2025-09-02 18:20:25 +0900

    Feat : "SpikeTrap"

--- Source/FF44/Interactable/FF44SpikeTrap.cpp
+++ Source/FF44/Interactable/FF44SpikeTrap.cpp
@@ -2,4 +2,211 @@
 
 
 #include "Interactable/FF44SpikeTrap.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "GameFramework/Character.h"
 
+AFF44SpikeTrap::AFF44SpikeTrap()
+{
+    PrimaryActorTick.bCanEverTick = true;
+
+    SpikeRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SpikeRoot"));
+    RootComponent = SpikeRoot;
+
+    if (Mesh)
+    {
+        Mesh->SetupAttachment(SpikeRoot);
+        Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+        Mesh->SetHiddenInGame(true);
+    }
+
+    BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMesh"));
+    BaseMesh->SetupAttachment(RootComponent);
+    BaseMesh->SetCollisionProfileName(TEXT("BlockAll"));
+
+    SpikeMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SpikeMesh"));
+    SpikeMesh->SetupAttachment(SpikeRoot);
+    SpikeMesh->SetCollisionProfileName(TEXT("NoCollision"));
+
+    DamageZone = CreateDefaultSubobject<UBoxComponent>(TEXT("DamageZone"));
+    DamageZone->SetupAttachment(SpikeMesh);
+    DamageZone->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    DamageZone->SetCollisionResponseToAllChannels(ECR_Ignore);
+    DamageZone->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+    DamageZone->SetGenerateOverlapEvents(true);
+}
+
+void AFF44SpikeTrap::BeginPlay()
+{
+    Super::BeginPlay();
+
+    SpikeDownLocal = SpikeMesh->GetRelativeLocation();
+    SpikeUpLocal = SpikeDownLocal + FVector(0, 0, SpikeRaiseDistance);
+
+    if (TriggerBox)
+    {
+        TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &AFF44SpikeTrap::OnTriggerBegin);
+        TriggerBox->OnComponentEndOverlap.AddDynamic(this, &AFF44SpikeTrap::OnTriggerEnd);
+    }
+}
+
+void AFF44SpikeTrap::OnTriggerBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+    bool bFromSweep, const FHitResult& Sweep)
+{
+    if (!OtherActor || bDisarmed) return;
+
+    if (Cast<APawn>(OtherActor))
+    {
+        OverlapCount++;
+
+        if (!bCycling)
+        {
+            StartCycle();
+        }
+    }
+}
+
+void AFF44SpikeTrap::OnTriggerEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+    if (!OtherActor || bDisarmed) return;
+
+    if (Cast<APawn>(OtherActor))
+    {
+        OverlapCount = FMath::Max(0, OverlapCount - 1);
+    }
+}
+
+bool AFF44SpikeTrap::CanInteract_Implementation(AActor* Interactor) const
+{
+    return !bDisarmed;
+}
+
+void AFF44SpikeTrap::Interact_Implementation(AActor* Interactor)
+{
+    if (bDisarmed) return;
+
+    bDisarmed = true;
+    bCycling = false;
+
+    GetWorldTimerManager().ClearTimer(DelayHandle);
+    GetWorldTimerManager().ClearTimer(HoldHandle);
+    GetWorldTimerManager().ClearTimer(CooldownHandle);
+
+    SpikeRoot->SetRelativeLocation(SpikeDownLocal);
+
+    if (DamageZone)
+    {
+        DamageZone->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    }
+    if (TriggerBox)
+    {
+        TriggerBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    }
+
+    SetPromptVisible(false);
+}
+
+void AFF44SpikeTrap::StartCycle()
+{
+    if (bDisarmed || bCycling) return;
+
+    bCycling = true;
+
+    GetWorldTimerManager().SetTimer(DelayHandle, this, &AFF44SpikeTrap::RaiseSpikes, TriggerDelay, false);
+}
+
+void AFF44SpikeTrap::RaiseSpikes()
+{
+    if (bDisarmed)
+    {
+        bCycling = false;
+        return;
+    }
+
+    FLatentActionInfo LatentInfo;
+    LatentInfo.CallbackTarget = this;
+    LatentInfo.ExecutionFunction = FName("OnRaised");
+    LatentInfo.Linkage = 0;
+    LatentInfo.UUID = 1001;
+
+    UKismetSystemLibrary::MoveComponentTo(
+        SpikeMesh,
+        SpikeUpLocal,
+        SpikeMesh->GetRelativeRotation(),
+        bEaseOut,
+        bEaseIn,
+        RaiseTime,
+        false,
+        EMoveComponentAction::Move,
+        LatentInfo
+    );
+}
+
+void AFF44SpikeTrap::RetractSpikes()
+{
+    if (bDisarmed)
+    {
+        bCycling = false;
+        return;
+    }
+
+    FLatentActionInfo LatentInfo;
+    LatentInfo.CallbackTarget = this;
+    LatentInfo.ExecutionFunction = FName("OnRetracted");
+    LatentInfo.Linkage = 0;
+    LatentInfo.UUID = 1002;
+
+    UKismetSystemLibrary::MoveComponentTo(
+        SpikeMesh,
+        SpikeDownLocal,
+        SpikeMesh->GetRelativeRotation(),
+        bEaseOut,
+        bEaseIn,
+        LowerTime,
+        false,
+        EMoveComponentAction::Move,
+        LatentInfo
+    );
+}
+
+void AFF44SpikeTrap::EndCooldown()
+{
+    bCycling = false;
+
+    if (!bDisarmed && OverlapCount > 0)
+    {
+        StartCycle();
+    }
+}
+
+void AFF44SpikeTrap::ApplyDamageOnce()
+{
+    if (!DamageZone) return;
+
+    TArray<AActor*> Overlapping;
+    DamageZone->GetOverlappingActors(Overlapping, APawn::StaticClass());
+
+    for (AActor* A : Overlapping)
+    {
+        if (!A) continue;
+        UGameplayStatics::ApplyDamage(A, Damage, nullptr, this, DamageType ? *DamageType : nullptr);
+    }
+
+    // 필요하면 소리/파티클 재생 지점
+}
+
+void AFF44SpikeTrap::OnRaised()
+{
+    ApplyDamageOnce();
+
+    GetWorldTimerManager().SetTimer(HoldHandle, this, &AFF44SpikeTrap::RetractSpikes, HoldTime, false);
+}
+
+void AFF44SpikeTrap::OnRetracted()
+{
+    GetWorldTimerManager().SetTimer(CooldownHandle, this, &AFF44SpikeTrap::EndCooldown, CooldownTime, false);
+}
--- Source/FF44/Interactable/FF44SpikeTrap.h
+++ Source/FF44/Interactable/FF44SpikeTrap.h
@@ -6,12 +6,98 @@
 #include "Interactable/FF44TrapBase.h"
 #include "FF44SpikeTrap.generated.h"
 
-/**
- * 
- */
+class UStaticMeshComponent;
+class UBoxComponent;
+
 UCLASS()
 class FF44_API AFF44SpikeTrap : public AFF44TrapBase
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    AFF44SpikeTrap();
+
+protected:
+    virtual void BeginPlay() override;
+
+    UFUNCTION()
+    void OnTriggerBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+        bool bFromSweep, const FHitResult& Sweep);
+
+    UFUNCTION()
+    void OnTriggerEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+    virtual bool CanInteract_Implementation(AActor* Interactor) const override;
+    virtual void Interact_Implementation(AActor* Interactor) override;
+
+private:
+    void StartCycle();
+    void RaiseSpikes();
+    void RetractSpikes();
+    void EndCooldown();
+
+    void ApplyDamageOnce();
+
+private:
+    UPROPERTY(VisibleAnywhere, Category = "SpikeTrap|Components")
+    USceneComponent* SpikeRoot;
+
+    UPROPERTY(VisibleAnywhere, Category = "SpikeTrap|Components")
+    UStaticMeshComponent* BaseMesh;
+
+    UPROPERTY(VisibleAnywhere, Category = "SpikeTrap|Components")
+    UStaticMeshComponent* SpikeMesh;
+
+    UPROPERTY(VisibleAnywhere, Category = "SpikeTrap|Components")
+    UBoxComponent* DamageZone;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SpikeTrap|Timing", meta = (AllowPrivateAccess = "true", ClampMin = "0"))
+    float TriggerDelay = 0.25f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SpikeTrap|Timing", meta = (AllowPrivateAccess = "true", ClampMin = "0"))
+    float HoldTime = 0.5f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SpikeTrap|Timing", meta = (AllowPrivateAccess = "true", ClampMin = "0"))
+    float CooldownTime = 0.8f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SpikeTrap|Motion", meta = (AllowPrivateAccess = "true"))
+    float SpikeRaiseDistance = 60.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SpikeTrap|Motion", meta = (AllowPrivateAccess = "true", ClampMin = "0.01"))
+    float RaiseTime = 0.20f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SpikeTrap|Motion", meta = (AllowPrivateAccess = "true", ClampMin = "0.01"))
+    float LowerTime = 0.25f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SpikeTrap|Motion", meta = (AllowPrivateAccess = "true"))
+    bool bEaseIn = true;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SpikeTrap|Motion", meta = (AllowPrivateAccess = "true"))
+    bool bEaseOut = true;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SpikeTrap|Damage", meta = (AllowPrivateAccess = "true"))
+    float Damage = 25.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SpikeTrap|Damage", meta = (AllowPrivateAccess = "true"))
+    TSubclassOf<UDamageType> DamageType;
+
+    bool bCycling = false;
+    bool bDisarmed = false;
+
+    FVector SpikeDownLocal;
+    FVector SpikeUpLocal;
+
+    int32 OverlapCount = 0;
+
+    FTimerHandle DelayHandle;
+    FTimerHandle HoldHandle;
+    FTimerHandle CooldownHandle;
+
+    UFUNCTION()
+    void OnRaised();
+
+    UFUNCTION()
+    void OnRetracted();
 };
