a597f9658008b63ca8f37dec79a6cf1be5b5f8a9
lyysss1210 <lyysss1210@naver.com>
2025-09-11 03:08:29 +0900

    Feat : "ItemUse Class 제작 시작"

---
+++ Source/FF44/Player/Abilities/GA_Player_ItemUse.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GA_Player_ItemUse.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FF44_API UGA_Player_ItemUse : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
